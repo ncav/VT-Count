8faeec0c56f1c9aeb4d9c1938276ebf237ad6378
ncav <52970090+ncav@users.noreply.github.com>
2023-03-01 13:47:31 -0500

    Update and rename VT_Community.py to VT_SHA256_lookup.py

---
+++ VT_SHA256_lookup.py
@@ -0,0 +1,88 @@
+import requests
+
+api_key = "bff22ae9bdff6828e2b2c09b13c85c8480a17e0a7e5f40bb7f0aa53556b64265"
+
+while True:
+    resource = input("Enter SHA256 Hash (or type 'exit' to quit): ")
+    if resource == "exit":
+        break
+
+    def get_votes(api_key, resource):
+        url = f"https://www.virustotal.com/api/v3/files/{resource}/votes"
+        headers = {"x-apikey": api_key}
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
+    def get_comments(api_key, resource):
+        url = f"https://www.virustotal.com/api/v3/files/{resource}/comments"
+        headers = {"x-apikey": api_key}
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
+    def get_analysis(api_key, resource):
+        url = f"https://www.virustotal.com/api/v3/files/{resource}/analyse"
+        headers = {"x-apikey": api_key}
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
+    # Pull in votes, comments, and analysis results
+    votes = get_votes(api_key, resource)
+    comments = get_comments(api_key, resource)
+    analysis = get_analysis(api_key, resource)
+
+    # If there are no comments, Error will display. There must be at least one comment to pull into the directories
+    if comments is not None:
+        comment_count = 0
+        for comment in comments["data"]:
+            author = comment["attributes"].get("author", {})
+            if author:
+                username = author.get("user", "")
+                profile_url = author.get("url", "")
+            else:
+                username = ""
+                profile_url = ""
+            content = comment["attributes"]["text"]
+            print("-----------------Comments----------------")
+            print(f"{username} ({profile_url}): {content}")
+            comment_count += 1
+        print(f"Total comments: {comment_count}")
+    else:
+        print("Error getting comments")
+
+    # Votes of positive and negative must start at 0 or an error will display. Loop through the data and pull in the attributes of positive or negative
+    if votes is not None:
+        total_votes = 0
+        positive_votes = 0
+        negative_votes = 0
+        for vote in votes["data"]:
+            total_votes += vote["attributes"]["value"]
+            if vote["attributes"]["value"] > 0:
+                positive_votes += 1
+            else:
+                negative_votes += 1
+        print("-------------Vote Count-------------")
+        print(f"Total votes: {total_votes}")
+        print(f"Positive votes: {positive_votes}")
+        print(f"Negative votes: {negative_votes}")
+    else:
+        print("Error getting votes")
+
+    # Extract detection ratio from analysis results
+    if analysis is not None:
+        detection_ratio = analysis["data"]["attributes"]["last_analysis_stats"]["malicious"] / analysis["data"]["attributes"]["last_analysis_stats"]["total"]
+        print("-------------Detection Ratio-------------")
+
+        print("-------------Detection Ratio-------------")
+        print(f"Detection ratio: {detection_ratio}")
+    else:
+        print("Error getting analysis")
+#Detection reation needs paid API
